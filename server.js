d47dc81c9714e2e0d6d2561fb9a22ec4d97f747c
luisrnandezc <luisrnandezc@gmail.com>
2025-12-21 14:20:52 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+// Entry point of the Node.js OData service
+
+const express = require('express');
+const cors = require('cors');
+
+// Create Express application
+const app = express();
+
+// Allow calls from browser
+app.use(cors());
+
+// Parse JSON request bodies
+app.use(express.json());
+
+// Simple test endpoint
+app.get('/', (req, res) => {
+  res.send('Travel OData Service is running');
+});
+
+app.use('/odata/$metadata', require('./routes/metadata'));
+app.use('/odata/Travels', require('./routes/travels'));
+
+// Start server
+const PORT = 4004;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
