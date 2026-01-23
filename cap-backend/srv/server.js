329b6e891460f00608a34fee9dab853e764b6a0e
luisrnandezc <luisrnandezc@gmail.com>
2026-01-22 23:47:58 -0400

    fix: update server.js file with a more robust cors setting

--- cap-backend/srv/server.js
+++ cap-backend/srv/server.js
@@ -2,7 +2,14 @@ const cds = require('@sap/cds');
 const cors = require('cors');
 
 cds.on('bootstrap', (app) => {
-  app.use(cors()); // This enables CORS for ALL origins
+  app.use(
+    cors({
+      origin: '*', // Allows any origin
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  ); // This enables CORS for ALL origins
 });
 
 module.exports = cds.server;
