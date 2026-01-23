94a55e5892d874e9f157c5a6cad86ba59a167ffe
luisrnandezc <luisrnandezc@gmail.com>
2026-01-22 21:22:54 -0400

    feat: minor fixes to the onSearch button in the dashboard controller

--- fiori-frontend/webapp/controller/Dashboard.controller.ts
+++ fiori-frontend/webapp/controller/Dashboard.controller.ts
@@ -1,7 +1,11 @@
 /// <reference types="@openui5/ts-types" />
 
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller'],
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+  ],
 
   function (Controller: typeof sap.ui.core.mvc.Controller) {
     'use strict';
@@ -10,6 +14,31 @@ sap.ui.define(
       onInit() {
         console.log('Dashboard initiated');
       },
+
+      onSearch(oEvent: sap.ui.base.Event): void {
+        console.log('onSearch fired');
+        const sQuery = (oEvent.getSource() as sap.m.SearchField).getValue();
+        const oTable = this.byId('customersTable') as sap.m.Table;
+        console.log('Table:', oTable);
+        const oBinding = oTable.getBinding('items') as sap.ui.model.ListBinding;
+        console.log('oBinding:', oBinding);
+        console.log('Binding path:', oBinding.getPath());
+
+        if (!oBinding) {
+          return;
+        }
+
+        if (sQuery && sQuery.length > 0) {
+          const oFilter = new sap.ui.model.Filter(
+            'CustomerName',
+            sap.ui.model.FilterOperator.Contains,
+            sQuery,
+          );
+          oBinding.filter([oFilter]);
+        } else {
+          oBinding.filter([]);
+        }
+      },
     });
-  }
+  },
 );
